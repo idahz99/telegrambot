63e77d39f2bd784647d07e1b0ff13d9943563598
idahz99 <idahz99@gmail.com>
2021-12-18 20:50:17 +0800

    make it better

--- App_Pkob/models.py
+++ App_Pkob/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from django.utils import timezone
 # Create your models here.
 
 
 class People(models.Model):
-    Timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+    Timestamp = models.DateTimeField(auto_now_add=True, null=True,)
     IcNo = models.CharField(primary_key=True, max_length=12, blank=False)
     Name = models.CharField(max_length=255)
     Phone = models.CharField(max_length=10)
+

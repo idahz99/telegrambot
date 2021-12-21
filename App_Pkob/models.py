f65857ff4a81e873b89af22e198b157f8807e847
idahz99 <idahz99@gmail.com>
2021-12-21 17:51:54 +0800

    make telegram work timestamp

--- App_Pkob/models.py
+++ App_Pkob/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 class People(models.Model):
-    Timestamp = models.DateTimeField(auto_now_add=True, null=True,)
+    timestamp = models.DateTimeField(auto_now_add=True, null=True,)
     IcNo = models.CharField(primary_key=True, max_length=12, blank=False)
     Name = models.CharField(max_length=255)
     Phone = models.CharField(max_length=10)
